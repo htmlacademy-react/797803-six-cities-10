e1faf5d0c6eaf33eea507d1fae84d6ee2e161604
ageevDmitry <ageevdmitry2018@gmail.com>
2022-07-19 09:31:21 +0300

    Refactor ReviewForm

--- project/src/components/review-form/review-form.tsx
+++ project/src/components/review-form/review-form.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {Fragment} from 'react';
 import {useState, ChangeEvent} from 'react';
 import {REVIEW_FORM_STATUS} from '../../const';
 
@@ -9,7 +9,7 @@ function ReviewForm(): JSX.Element {
     rating: null,
   });
 
-  const fieldChangeHadle = (evt: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
+  const handleFormReviewChange = (evt: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
     const {name, value} = evt.target;
     setFormData({...formData, [name]: value});
   };
@@ -19,17 +19,17 @@ function ReviewForm(): JSX.Element {
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
         {REVIEW_FORM_STATUS.map((item) => (
-          <React.Fragment key = {item.startNumber}>
-            <input onChange={fieldChangeHadle} className="form__rating-input visually-hidden" name="rating" defaultValue={item.startNumber} id={`${item.startNumber}-stars`} type="radio" />
+          <Fragment key = {item.startNumber}>
+            <input onChange={handleFormReviewChange} className="form__rating-input visually-hidden" name="rating" defaultValue={item.startNumber} id={`${item.startNumber}-stars`} type="radio" />
             <label htmlFor={`${item.startNumber}-stars`} className="reviews__rating-label form__rating-label" title={item.title}>
               <svg className="form__star-image" width={37} height={33}>
                 <use xlinkHref="#icon-star" />
               </svg>
             </label>
-          </React.Fragment>
+          </Fragment>
         ))}
       </div>
-      <textarea onChange={fieldChangeHadle} className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" defaultValue={''} />
+      <textarea onChange={handleFormReviewChange} className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" defaultValue={''} />
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
                         To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
